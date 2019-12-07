739cb3c2e47facb0d847c0e345845eb2566c7bfe
sudnonk <sudnonk12@yahoo.co.jp>
2019-12-08 00:48:47 +0900

    #を付けるのを忘れてた

--- web/main.js
+++ web/main.js
@@ -1,18 +1,17 @@
 (() => {
-    const prev = $("prev");
-    const start = $("start");
-    const next = $("next");
-    const top_button = $("top");
+    const prev = $("#prev");
+    const start = $("#start");
+    const next = $("#next");
+    const top_button = $("#top");
 
-    const info_step = $("step");
-    const info_cells = $("cells");
+    const info_step = $("#step");
+    const info_cells = $("#cells");
 
-    const canvas = $("canvas");
+    const canvas = $("#canvas");
 
     let step = 0;
 
     prev.on("click", () => {
-        console.log($(this).innerText);
         step--;
         if (step < 0) {
             step = 0;
