e196bc6b5332b1b60b10dc7697569d9be8aa4df6
sudnonk <sudnonk12@yahoo.co.jp>
2019-12-08 00:10:59 +0900

    取得したjsonを表示してみる

--- web/main.js
+++ web/main.js
@@ -3,11 +3,21 @@ const start = document.getElementById("start");
 const next = document.getElementById("next");
 const top_button = document.getElementById("top");
 
+const info_step = document.getElementById("step");
+const info_cells = document.getElementById("cells");
+
+const canvas = document.getElementById("canvas");
+
 let step = 0;
 
 prev.onclick = (ev) => {
     step--;
-    const json = get_json(step);
+    get_json(step)
+        .then((json) => {
+            console.log(json)
+            show_info(json);
+            visualize(json);
+        })
 };
 
 next.onclick = (ev) => {
@@ -21,12 +31,23 @@ top_button.onclick = (ev) => {
     const json = get_json(step);
 };
 
-function get_json(step) {
-    fetch('get.php?n=' + step)
+async function get_json(step) {
+    return fetch('get.php?n=' + step)
         .then((response) => response.json())
         .catch((error) => console.error(error))
 }
 
+/**
+ * @param json Object
+ */
 function show_info(json) {
+    const num = json.Cells.length;
+    console.log(num);
+
+    info_step.innerText = step;
+    info_cells.innerText = num;
+}
+
+function visualize(json) {
 
 }
