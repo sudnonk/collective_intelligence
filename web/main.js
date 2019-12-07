f2db758879960a20da7e14d4692888bcf9f1c435
sudnonk <sudnonk12@yahoo.co.jp>
2019-12-08 01:32:56 +0900

    textが表示されない

--- web/main.js
+++ web/main.js
@@ -58,9 +58,9 @@ function show_info(json) {
     circles.on("click", function () {
         const id = $(this).attr("id").slice(2);
         const cell = json.Cells[id];
-        fearness.innerText = cell.persona.fear.toString();
-        kindness.innerText = cell.persona.kindness.toString();
-        resource.innerText = cell.resource.toString();
+        $(fearness).text(cell.persona.fear.toString());
+        $(kindness).text(cell.persona.kindness.toString());
+        $(resource).text(cell.resource.toString());
     });
     paths.on("click", function () {
         const id = $(this).attr("id").slice(2);
